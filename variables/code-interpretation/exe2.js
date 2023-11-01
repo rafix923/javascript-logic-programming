88338c5b19963576e1e28d3a0faabfb6c1a4e153
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-11-01 10:18:13 -0300

    Restore code-interpratation file from variables file

---
+++ variables/code-interpretation/exe2.js
@@ -0,0 +1,13 @@
+/* Exercícios de interpretação de código 2 de 3
+
+Analise o programa abaixo e diga o que será impresso no console, SEM EXECUTAR o programa. */
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+// -> 10 10 10
---
+++ variables/code-interpretation/exe3.js
@@ -0,0 +1,15 @@
+/* Exercícios de interpretação de código 3 de 3
+
+Analise o programa abaixo, entenda o que ele faz e sugira melhores nomes para as variáveis. 
+Lembre-se que devemos escolher nomes significativos, usar o padrão camelCase. 
+Alem disso, os nomes não podem começar com números ou caracteres especiais.
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t / p} por hora`) */
+
+
+// O programa abaixo calcula o valor que o usuário recebe por hora.
+let workingHoursDay = prompt("Quantas horas você trabalha por dia?")
+let earningPerDay = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${workingHoursDay / workingHoursDay} por hora`)
