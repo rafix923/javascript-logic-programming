aff1b8f341e809fa008f3ed009d5e97eec96d0a1
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-11-01 08:10:18 -0300

    Add the solution to exercise 2 of writing code

--- operators/code-writing-exercises/exe2.js
+++ operators/code-writing-exercises/exe2.js
@@ -2,3 +2,15 @@
 
 Faça um programa que: */
 
+// a) Peça ao usuário que insira um número par
+const num = prompt("Insira um número par");
+
+// b) Imprima na console o resto da divisão desse número por 2.
+const reminder = num % 2;
+console.log(reminder);
+
+// c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+// R: O resto da divisão sempre será o número zero.
+
+// d) O que acontece se o usuário inserir um número ímpar? Escreva em um comentário de código
+// R: O resto da divisão sempre será o número um.
