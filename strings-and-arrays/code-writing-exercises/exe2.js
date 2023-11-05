a4f4fbc942dac03436a635500f407fc8dacceee1
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-11-04 21:55:00 -0300

    Add solution to part c of code writing exercise 2

--- strings-and-arrays/code-writing-exercises/exe2.js
+++ strings-and-arrays/code-writing-exercises/exe2.js
@@ -8,10 +8,20 @@ let favoriteDishes = ["feijoada", "macarronada", "pamonha", "pizza", "torta de c
 // a) Imprima no console o array completo
 console.log(favoriteDishes);
 
-/* b) Imprima no console a mensagem "Essas são as minhas comidas preferidas: ", 
-seguida por cada uma das comidas, uma embaixo da outra. */
+// b) Imprima no console a mensagem "Essas são as minhas comidas preferidas: ", 
+// seguida por cada uma das comidas, uma embaixo da outra.
+
 const message = "Essas são as minhas comidas preferidas:";
 console.log(message);
 favoriteDishes.forEach(dishe => {
     console.log(dishe);
 });
+
+// c) Aqui vai um desafio: pergunte ao usuário uma comida preferida.
+const userFavoriteDish = prompt("Qual sua comida favorita?");
+
+// Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima no console a nova lista.
+favoriteDishes[1] = userFavoriteDish;
+console.log(favoriteDishes);
+
+
