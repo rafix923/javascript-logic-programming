50337fcb6009ba25077626af5d7a295845e0fc47
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-11-07 00:37:34 -0300

    Add solution to challenge 2

---
+++ strings-and-arrays/challenges/challenge2.js
@@ -0,0 +1,8 @@
+/* Dado o array ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"], 
+faça um programa que acha o índice da palavra Abacaxi e imprime tanto o índice quanto o tamanho do array */
+
+let fruits = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+const indexOfAbacaxi = console.log(
+    "O índice da palavra Abacaxi é " + fruits.indexOf("Abacaxi") + ". " +
+    "E o Tamano do array é " + fruits.length + "."
+);
