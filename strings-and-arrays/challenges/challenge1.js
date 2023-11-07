5f2c13f7ad75759c4e3d8e1eb2d4d53e7fe54496
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-11-07 00:05:35 -0300

    Add solution to chalenge 1

---
+++ strings-and-arrays/challenges/challenge1.js
@@ -0,0 +1,8 @@
+// Desafio 1 de 2
+
+// Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços
+
+// const arrayOfWords = [];
+let phrase = "Vamos aprender lógica de programação com JavaScript";
+let arrayOfWorlds = phrase.split(" ");
+console.log(arrayOfWorlds);
